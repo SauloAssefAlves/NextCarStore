c1a1be40efb387e8822c1230d47093fef9b40a55
Saulo <saulo.assef98@gmail.com>
2024-09-23 19:23:09 -0300

    feat: validacao dos inputs de login

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,18 +1,31 @@
+import { RegisterOptions, UseFormRegister } from "react-hook-form";
 interface InputProps {
   type: string;
   placeholder: string;
   name: string;
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
 }
 
-export function Input({ name, placeholder, type }: InputProps) {
+export function Input({
+  name,
+  placeholder,
+  type,
+  register,
+  rules,
+  error,
+}: InputProps) {
   return (
     <div>
       <input
-        className='w-full border-2 rounded-md h11 px-2 outline-none'
+        className='w-full border-2 rounded-md h-11 px-2 outline-none'
         placeholder={placeholder}
-        name={name}
         type={type}
+        {...register(name, rules)}
+        id={name}
       />
+      {error && <p className="my-1 text-red-600">{error}</p>}
     </div>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,16 +2,62 @@ import { Link } from "react-router-dom";
 import logotipo from "../../assets/logotipo.png";
 import { Container } from "../../components/container";
 import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .min(1, "O campo de email obrigatório"),
+  password: z.string().min(1, "O campo senha é obrigatório"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
   return (
     <Container>
       <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
         <Link to='/' className='mb-6 max-w-sm w-full'>
           <img src={logotipo} alt='logotipo' className='w-full ' />
         </Link>
-        <form className='bg-white max-w-xl w-full rounded-lg'>
-          <Input type='email' placeholder='Digite seu email...' name='email' />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='bg-white max-w-xl w-full rounded-lg'>
+          <div className='mb-3'>
+            <Input
+              type='email'
+              placeholder='Digite seu email...'
+              name='email'
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+          <div className='mb-3'>
+            <Input
+              type='password'
+              placeholder='Digite sua senha...'
+              name='password'
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button>Acessar</button>
         </form>
       </div>
     </Container>
