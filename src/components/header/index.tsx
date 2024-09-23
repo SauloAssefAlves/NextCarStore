e99bbac9da7e37d495c71ec83659c641c55350dd
Saulo <saulo.assef98@gmail.com>
2024-09-22 21:11:11 -0300

    feat: adiciona a estilização da pagina home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,34 @@
+import { Link } from "react-router-dom";
+import logotipo from "../../assets/logotipo.png";
+import icone from "../../assets/icone.png";
+import { FiUser, FiLogIn } from "react-icons/fi";
 export function Header() {
+  const signed = true;
+  const loadingAuth = false;
+  let changeLogo = false
+
   return (
-    <>
-      <div>
-        <h1>header component</h1>
-      </div>
-    </>
+    <div className='w-full flex items-center justify-center h-20 bg-white drop-shadow mb-4'>
+      <header className='flex w-full items-center justify-between max-w-7xl px-4 mx-auto'>
+        <Link to='/'>
+          <img className='w-48 hidden sm:block' src={logotipo} />
+          <img className='w-14 sm:hidden' src={icone} />
+        </Link>
+        {!loadingAuth && signed && (
+          <Link to='/dashboard'>
+            <div className='border-2 rounded-full p-1 border-blue-900'>
+              <FiUser size={24} color='#000' />
+            </div>
+          </Link>
+        )}
+        {!loadingAuth && !signed && (
+          <Link to='/login'>
+            <div className='border-2 rounded-full p-1 border-gray-900'>
+              <FiLogIn size={24} color='#000' />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,47 @@
+import { Container } from "../../components/container";
+
 export function Home() {
   return (
     <>
-      <div>
-        <h1>Page Home</h1>
-      </div>
+      <Container>
+        <section className='bg-white p-4 rounded-lg max-w-3xl mx-auto flex justify-center items-center gap-2'>
+          <input
+            className='w-full border-2 rounded-lg h-9 px-3 outline-none'
+            placeholder='Digite o nome do carro...'
+          />
+          <button className='bg-blue-500 h-9 px-8 rounded-lg text-white font-semibold text-lg'>
+            Buscar
+          </button>
+        </section>
+        <h1 className='font-bold text-center mt-6 text-2xl mb-4'>
+          Carros novos e usados em todo o Brasil
+        </h1>
+
+        <main className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+          <section className='w-full bg-white rounded-lg'>
+            <img
+              className='w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all'
+              src='https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202407/20240730/mercedesbenz-gle-450d-3-0-i6-mhev-diesel-coupe-4matic-9gtronic-wmimagem12411489317.webp?s=fill&w=552&h=414&q=60'
+              alt='Carro'
+            />
+            <p className='font-bold mt-1 mb-2 px-2'>BMW 320i</p>
+            <div className='flex flex-col px-2'>
+              <span className='text-zinc-700 mb-6'>
+                Ano 2016/2016 | 23.000 km
+              </span>
+              <strong className='text-black font-medium text-xl'>
+                R$ 190.000
+              </strong>
+            </div>
+            <div className='w-full h-px bg-sky-200 my-2'></div>
+            <div className='px-2 pb-2'>
+              <span className='text-zinc-700'>Campo Grande - MS</span>
+            </div>
+          </section>
+    
+
+        </main>
+      </Container>
     </>
   );
 }
